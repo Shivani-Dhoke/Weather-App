c41482a0db7093326f3443c9982559a5ff62c573
Shivani-Dhoke <shivani.dhoke@eminds.ai>
2022-10-31 18:16:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-import Weather from './components/Weather';
-
+import Weather from './components/weather';
+import {useState} from 'react'
 function App() {
+  const [input, setInput] = useState("")
+ 
+  const handleClick=()=>{
+    alert(input)
+  
+  }
+  const handleChange=(e)=>{
+setInput(e.target.value)
+  }
+ 
+ 
+  
   return (
     <div>
+      <input className={input ? "textRed":'textBlue'} type="text" onChange={handleChange} />
+      <button  onClick={handleClick}>Search</button>
       <Weather/>
+
     </div>
   );
 }
